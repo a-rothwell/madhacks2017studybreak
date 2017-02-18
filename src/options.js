e5e25638348bc11d7ccf98c49dc42f111e1e00da
Andrew Rothwell <a_rothwell@outlook.com>
2017-02-18 16:34:47 -0600

    Added external css file for options

--- src/options.js
+++ src/options.js
@@ -1,16 +1,13 @@
 window.onLoad = function() {
-  var minutes = document.getElementById("minutes").value
-  var interval = document.getElementById("interval").value
-  document.getElementById("saveChanges").addEventListener("click", saveChanges);
-
-  function saveChanges() {
-    chrome.storage.sync.set({'minutes': minutes , 'interval': interval});
-  }
-
-  function load() {
     chrome.storage.sync.get(['minutes', 'interval'], function(value) {
-      minutes = value.minutes;
-      interval = value.hours;
+        document.getElementById("minutes").value = value.minutes;
+        document.getElementById("interval").value = value.interval;
     });
+    
+  document.getElementById("savebutton").onclick = function saveChanges() {
+      chrome.storage.sync.set({
+          'minutes': document.getElementById("minutes").value,
+          'interval': document.getElementById("interval").value
+      });
   }
-};
+}
