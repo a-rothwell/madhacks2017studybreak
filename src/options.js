ad89b8bf8093199a2a52a335a3a08f4b8c92fdc0
Andrew Rothwell <a_rothwell@outlook.com>
2017-02-18 16:01:15 -0600

    Added external css file for options

--- src/options.js
+++ src/options.js
@@ -1,32 +1,16 @@
-// Saves options to chrome.storage
-function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
-  chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
-}
+window.onLoad = function() {
+  var minutes = document.getElementById("minutes").value
+  var interval = document.getElementById("interval").value
+  document.getElementById("saveChanges").addEventListener("click", saveChanges);
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
-  });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-    save_options);
+  function saveChanges() {
+    chrome.storage.sync.set({'minutes': minutes , 'interval': interval});
+  }
+
+  function load() {
+    chrome.storage.sync.get(['minutes', 'interval'], function(value) {
+      minutes = value.minutes;
+      interval = value.hours;
+    });
+  }
+};
