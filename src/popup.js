3d5d47991a1389e448e4b15475e6303da03d493d
lshanker <lshanke@purdue.edu>
2017-02-18 17:59:50 -0600

    working on reading in the links

--- src/popup.js
+++ src/popup.js
@@ -1,13 +1,12 @@
-chrome.storage.sync.set({
-    'link': 'http://www.google.com'
-}, function() {
 
-});
-
-chrome.storage.sync.get('link', function(value) {
-    chrome.tabs.create({
-        url: value.link
-    })
+chrome.storage.sync.get('links', function(value) {
+  document.write(value.links[0]);
+  /*
+    for(var i = 0; i<(value.links).length; i++){
+        document.write(i + "\n");
+        document.write((value.links)[i]);
+    }
+    */
 });
 
 document.write("HERE");
