673a399f8712b821897d22ce0bbbea45f059b79d
Andrew Rothwell <a_rothwell@outlook.com>
2017-02-18 21:28:01 -0600

    add moment lib

--- src/popup.js
+++ src/popup.js
@@ -1,22 +1,35 @@
-chrome.storage.sync.get('link', function(value) {
-    chrome.tabs.create({
-        url: value.link
-    });
-});
-
-var id;
 chrome.tabs.getSelected(function(tab) {
-    id = tab.id
+    url = tab.url;
+    console.log(url);
 });
-
-chrome.storage.sync.get('minutes', function(value) {
-    console.log(id);
-    console.log(value.minutes * 60000);
-    var myVar = setInterval(remove, value.minutes * .00005);
+chrome.storage.sync.get('link', function(value) {
+    gameUrl = value.link;
+    console.log(gameUrl);
 });
-
-function remove() {
-    chrome.tabs.getCurrent(function(tab) {
-        chrome.tabs.remove(id, function() {});
-    });
+console.log((new String(url) == new String(gameUrl)));
+if (new String(url) == new String(gameUrl)) {
+    // chrome.storage.sync.get('link', function(value) {
+    //     chrome.tabs.create({
+    //         url: value.link
+    //     });
+    // });
+} else {
+    
 }
+//
+// var id;
+// chrome.tabs.getSelected(function(tab) {
+//     id = tab.id
+// });
+//
+// chrome.storage.sync.get('minutes', function(value) {
+//     console.log(id);
+//     console.log(value.minutes * 60000);
+//     var myVar = setInterval(remove, value.minutes * .00005);
+// });
+//
+// function remove() {
+//     chrome.tabs.getCurrent(function(tab) {
+//         chrome.tabs.remove(id, function() {});
+//     });
+// }
