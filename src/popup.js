4729c9ebcb91af68721b65ee7d1ae907ad9ebb9c
Andrew Rothwell <a_rothwell@outlook.com>
2017-02-18 20:24:15 -0600

    new tab work

--- src/popup.js
+++ src/popup.js
@@ -1,3 +1,22 @@
 chrome.storage.sync.get('link', function(value) {
-  chrome.tabs.create({ url: value.link });
+    chrome.tabs.create({
+        url: value.link
+    });
 });
+
+var id;
+chrome.tabs.getSelected(function(tab) {
+    id = tab.id
+});
+
+chrome.storage.sync.get('minutes', function(value) {
+    console.log(id);
+    console.log(value.minutes * 60000);
+    var myVar = setInterval(remove, value.minutes * .00005);
+});
+
+function remove() {
+    chrome.tabs.getCurrent(function(tab) {
+        chrome.tabs.remove(id, function() {});
+    });
+}
