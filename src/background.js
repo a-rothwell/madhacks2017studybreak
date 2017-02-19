4048f6ab5c2de9b68689cc342722b867186347b9
lshanker <lshanke@purdue.edu>
2017-02-18 22:52:55 -0600

    Interval works now too (all basic functions complete!)

--- src/background.js
+++ src/background.js
@@ -1,12 +1,22 @@
 chrome.tabs.onUpdated.addListener(function(tab) {
       chrome.tabs.getSelected(function(tab){
-        chrome.storage.sync.get(['link', 'minutes'], function(value){
-          if(tab.url == value.link){
-            setTimeout(redirect, 60000*value.minutes);
+        chrome.storage.sync.get(['link', 'minutes', 'canPlay', 'interval'], function(value){
+            if(tab.url == value.link){
+              if(value.canPlay == 'no'){
+                chrome.tabs.update(tab.id, {url: "blocked.html"});
+              }else{
+                setTimeout(redirect, 60000*value.minutes);
 
-            function redirect(){
-              chrome.tabs.update(tab.id, {url: "blocked.html"});
-            }
+                function redirect(){
+                  chrome.storage.sync.set({'canPlay': "no"});
+                  chrome.tabs.update(tab.id, {url: "blocked.html"});
+                  setTimeout(resetCanPlay, 60000*value.interval);
+                }
+
+                function resetCanPlay(){
+                  chrome.storage.sync.set({'canPlay': "yes"});
+                }
+          }
           }
         });
       });
