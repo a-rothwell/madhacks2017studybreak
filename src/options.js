0c762e33c32d7ae26ceca13bd71b6dbf065548e6
Andrew Rothwell <a_rothwell@outlook.com>
2017-02-19 00:01:13 -0600

    update

--- src/options.js
+++ src/options.js
@@ -1,24 +1,33 @@
 document.addEventListener('DOMContentLoaded', function() {
-  //console.log('foo')
     chrome.storage.sync.get(['minutes', 'interval'], function(value) {
-        //console.log(value)
-        if(value.minutes!='undefined'){
-        document.getElementById("minutes").value = value.minutes;
+        if (value.minutes != 'undefined') {
+            document.getElementById("minutes").value = value.minutes;
         }
-
-      if(value.interval!='undefined'){
-        document.getElementById("interval").value = value.interval;
-      }
+        if (value.interval != 'undefined') {
+            document.getElementById("interval").value = value.interval;
+        }
     });
 
-  document.getElementById("savebutton").onclick = function saveChanges() {
-      document.getElementById("alert").innerHTML = "<div class = 'alert alert-success fade in'><strong>Changes Saved</strong></div>"
-      chrome.storage.sync.set({'link': document.getElementById("addValue").value});
-      chrome.storage.sync.set({
-          'minutes': document.getElementById("minutes").value,
-          'interval': document.getElementById("interval").value
-      });
-  }
+    document.getElementById("savebutton").onclick = function saveChanges() {
+        document.getElementById("alert").innerHTML = "<div class = 'alert alert-success fade in'><strong>Changes Saved</strong></div>"
+        chrome.storage.sync.set({
+            'link': document.getElementById("addValue").value
+        });
+        chrome.storage.sync.set({
+            'minutes': document.getElementById("minutes").value,
+            'interval': document.getElementById("interval").value
+        });
+    }
+    document.getElementById("iobutton").onclick = function toggle() {
+        var elem = document.getElementById("iobutton");
+        if (elem.value == "ON") {
+            elem.value = "OFF";
+            elem.className = "btn btn-danger"
+        } else {
+            elem.value = "ON";
+            elem.className = "btn btn-success"
+        }
+    }
 
 
 })
