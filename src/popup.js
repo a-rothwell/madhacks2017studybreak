c4d2e8aa8d41bf3a18659e6bd3e3e497f31681c4
Andrew Rothwell <a_rothwell@outlook.com>
2017-02-18 22:04:06 -0600

    hotfix

--- src/popup.js
+++ src/popup.js
@@ -1,37 +1,5 @@
-var moment = require('moment');
-document.getElementById("time").value = moment().valueOf();
-chrome.tabs.getSelected(function(tab) {
-    url = tab.url;
-    console.log(url);
-});
 chrome.storage.sync.get('link', function(value) {
-    gameUrl = value.link;
-    console.log(gameUrl);
+    chrome.tabs.create({
+        url: value.link
+    });
 });
-console.log((new String(url) == new String(gameUrl)));
-if (new String(url) == new String(gameUrl)) {
-    // chrome.storage.sync.get('link', function(value) {
-    //     chrome.tabs.create({
-    //         url: value.link
-    //     });
-    // });
-} else {
-
-}
-//
-// var id;
-// chrome.tabs.getSelected(function(tab) {
-//     id = tab.id
-// });
-//
-// chrome.storage.sync.get('minutes', function(value) {
-//     console.log(id);
-//     console.log(value.minutes * 60000);
-//     var myVar = setInterval(remove, value.minutes * .00005);
-// });
-//
-// function remove() {
-//     chrome.tabs.getCurrent(function(tab) {
-//         chrome.tabs.remove(id, function() {});
-//     });
-// }
