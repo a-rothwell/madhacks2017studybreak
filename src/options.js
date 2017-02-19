50aba3ec217d019760b5a8d6c139b3d5eb88c3d2
lshanker <lshanke@purdue.edu>
2017-02-18 18:47:52 -0600

    Cleans up the options page

--- src/options.js
+++ src/options.js
@@ -2,26 +2,22 @@ document.addEventListener('DOMContentLoaded', function() {
   //console.log('foo')
     chrome.storage.sync.get(['minutes', 'interval'], function(value) {
         //console.log(value)
+        if(value.minutes!='undefined'){
         document.getElementById("minutes").value = value.minutes;
+        }
+
+      if(value.interval!='undefined'){
         document.getElementById("interval").value = value.interval;
+      }
     });
 
   document.getElementById("savebutton").onclick = function saveChanges() {
+      chrome.storage.sync.set({'link': document.getElementById("addValue").value});
       chrome.storage.sync.set({
           'minutes': document.getElementById("minutes").value,
           'interval': document.getElementById("interval").value
       });
   }
 
-  document.getElementById("clear").onclick = function clear(){
-    chrome.storage.sync.clear();
-  }
-
-  document.getElementById("addGame").onclick = function addNewGame() {
-
-    chrome.storage.sync.set({'link': document.getElementById("addValue").value});
-
-
-  }
 
 })
